cb173bebf61b3c35c2b5d9ca44268a456fadc712
ArdaEtsyMentor <ardaetsymentor@outlook.com>
2024-06-10 21:27:30 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,76 +1,78 @@
 $(document).ready(function() {
+    /* Navigation burger onclick side navigation show */
+    $('.burger-container').on('click', function() {
+        $('.main-navigation').toggle('slow');
 
-	
-	/* Navigation burger onclick side navigation show */
-	$('.burger-container').on('click', function() {
-		$('.main-navigation').toggle('slow');
+        if($('#myBtn').hasClass('change')) {
+            $('body').addClass('stop-scroll');
+        } else {
+            $('body').removeClass('stop-scroll');
+        }
+    });
 
-		if($('#myBtn').hasClass('change')) {
-			$('body').addClass('stop-scroll');
-		} else {
-			$('body').removeClass('stop-scroll');
-		}
-	});
+    /* Smooth scroll */
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+           
+            }, 1000); // 1000 ms = 1 saniye, geçiş süresini ayarlayabilirsiniz
+        }
+    });
 
+    /* About me slider */
+    $('.about-me-slider').slick({
+        slidesToShow: 1,
+        prevArrow: '<span class="span-arrow slick-prev"><</span>',
+        nextArrow: '<span class="span-arrow slick-next">></span>'
+    });
 
-	/* About me slider */
-	$('.about-me-slider').slick({
-		slidesToShow: 1,
-		prevArrow: '<span class="span-arrow slick-prev"><</span>',
-		nextArrow: '<span class="span-arrow slick-next">></span>'
-	});
-
-	/* Blog slider */
-	$('.blog-slider').slick({
-		slidesToShow: 2,
-		prevArrow: '<span class="span-arrow slick-prev"><</span>',
-		nextArrow: '<span class="span-arrow slick-next">></span>',
-		responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 1
-			}
-		}
-		]
-	});
-	
+    /* Blog slider */
+    $('.blog-slider').slick({
+        slidesToShow: 2,
+        prevArrow: '<span class="span-arrow slick-prev"><</span>',
+        nextArrow: '<span class="span-arrow slick-next">></span>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
-
-
 
 var counta = 0;
 
 $(window).scroll(function(e){
-
-
-	/* Onscroll number counter */
-	var statisticNumbers = $('.single-count');
-	if(statisticNumbers.length) {
-		var oTop = statisticNumbers.offset().top - window.innerHeight;
-		if (counta == 0 && $(window).scrollTop() > oTop) {
-			$('.count').each(function() {
-				var $this = $(this),
-				countTo = $this.attr('data-count');
-				$({
-					countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				},
-
-				{
-					duration: 2000,
-					easing: 'swing',
-					step: function() {
-						$this.text(Math.floor(this.countNum));
-					},
-					complete: function() {
-						$this.text(this.countNum);
-					}
-				});
-			});
-			counta = 1;
-		}
-	}
-
+    /* Onscroll number counter */
+    var statisticNumbers = $('.single-count');
+    if(statisticNumbers.length) {
+        var oTop = statisticNumbers.offset().top - window.innerHeight;
+        if (counta == 0 && $(window).scrollTop() > oTop) {
+            $('.count').each(function() {
+                var $this = $(this),
+                countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                    countNum: countTo
+                },
+                {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function() {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function() {
+                        $this.text(this.countNum);
+                    }
+                });
+            });
+            counta = 1;
+        }
+    }
 });
